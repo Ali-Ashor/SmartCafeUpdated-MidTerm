4d45874e1380be8fbd71723c2927e70b00c74bb6
Shujaat Ali <166167818+Ali-Ashor@users.noreply.github.com>
2026-05-24 13:41:09 +0500

    Delete menu_item.py

--- menu_item.py
+++
@@ -1,8 +0,0 @@
-class MenuItem:
-    def __init__(self, name, price, category):
-        self.name = name
-        self.price = price
-        self.category = category
-
-    def display(self):
-        print(f"{self.name} - Rs.{self.price} ({self.category})")
